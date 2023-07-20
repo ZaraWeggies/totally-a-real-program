1d6d4b7ec5c9a20d0d64c2d2036d00f62090ddeb
Zara Daily <zara.daily@wegmans.com>
2023-07-20 10:50:20 -0400

    Beta 1 sounds right

--- RealProgram/RealProgram/src/Program.cs
+++ RealProgram/RealProgram/src/Program.cs
@@ -6,27 +6,86 @@ namespace RealProgram.src
     {
         static void Main(string[] args)
         {
-            Person zara = new Person();
-            Job softwareEngineer = new Job();
-            Gender woman = new Gender();
-            Gender man = new Gender();
-            Gender nonbinary = new Gender();
+            //Create some sample genders
+            Gender woman = new Gender() { Pronoun = "She/Her" };
+            Gender man = new Gender() { Pronoun = "He/Him" };
+            Gender nonbinary = new Gender() { Pronoun = "They/Them" };
 
-            woman.Pronoun = "She/Her";
-            man.Pronoun = "He/Him";
-            nonbinary.Pronoun = "They/Them";
+            //Create some jobs
+            Job softwareEngineer = new Job()
+            {
+                Title = "Software Engineer",
+                Salary = 900000,
+                Location = "Remote",
+                YearsExperience = 8
+            };
 
-            softwareEngineer.Title = "Software Engineer";
-            softwareEngineer.Salary = 900000;
-            softwareEngineer.Location = "Remote";
-            softwareEngineer.YearsExperience = 100;
+            Job teacher = new Job()
+            {
+                Title = "Teacher",
+                Salary = 30000,
+                Location = "Tupper Lake, NY",
+                YearsExperience = 1
+            };
 
-            zara.Name = "Zara";
-            zara.Age = 400;
-            zara.Job = softwareEngineer;
-            zara.Gender = woman;
+            Job unemployed = new Job()
+            {
+                Title = "Unemployed",
+                Salary = 0,
+                Location = "None",
+                YearsExperience = 0
+            };
 
-            Console.WriteLine("Hello, World!");
+            Job dentist = new Job()
+            {
+                Title = "Dentist",
+                Salary = 230000,
+                Location = "New York, NY",
+                YearsExperience = 4
+            };
+
+            //Create a list of people and add some people to it
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person()
+            {
+                Name = "Zara",
+                Age = 400,
+                Job = softwareEngineer,
+                Gender = woman
+            });
+
+            people.Add(new Person()
+            {
+                Name = "Joseph",
+                Age = 25,
+                Job = dentist,
+                Gender = man
+            });
+
+            people.Add(new Person()
+            {
+                Name = "Kevin",
+                Age = 21,
+                Job = unemployed,
+                Gender = man
+            });
+
+            people.Add(new Person()
+            {
+                Name = "Kim",
+                Age = 35,
+                Job = teacher,
+                Gender = nonbinary
+            });
+
+            //Print out the people
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.Name + " is a " + person.Age + "-year-old person whose pronouns are '" + 
+                    person.Gender.Pronoun + "'. Their current job title is '" + person.Job.Title +"' and they make $" + 
+                    person.Job.Salary.ToString() + "/year.");
+            }
         }
     }
 }
--- RealProgram/RealProgram/test/ProgramTest.cs
+++ RealProgram/RealProgram/test/ProgramTest.cs
@@ -14,21 +14,5 @@ namespace RealProgram.test
         {
             _sut = new Program();
         }
-
-        //[Fact]
-        //public void Main_ShouldWriteHelloWorld_WhenCalled()
-        //{
-        //    // Arrange
-        //    var console = Substitute.For<IConsole>();
-        //    _sut.Console = console;
-
-        //    // Act
-        //    _sut.Main(new string[] { });
-
-        //    // Assert
-        //    console.Received().WriteLine("Hello, World!");
-        //}
-
-
     }
 }
